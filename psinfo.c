91bb3af45a0114f8a3d15b2c5c01bf2e35fc258c
Sebastian Osorno Zapata <osorno.sebastian@gmail.com>
2018-04-01 13:23:02 -0500

    Proyecto finalizado con validaciones de error

--- psinfo.c
+++ psinfo.c
@@ -25,11 +25,14 @@ typedef struct Proceso Proceso;
 
 //Variables globales a utilizar.
 FILE *fpr;
+FILE *fp;
+char ruta[80];
 
 //Definición de las funciones a utilizar.
 void procesarProceso(char *, int);
 void imprimirProceso(Proceso);
 void guardarProceso(Proceso);
+int  validararchivo(char *);
 
 //Método principal.
 int main(int argc, char *argv[]){
@@ -45,8 +48,12 @@ int main(int argc, char *argv[]){
     char nombreArchivo[50]; //Nombre del archivo que se creará.
     strcpy(nombreArchivo, "psinfo-report");
     for(int index = 2; index < argc; ++index){
-        strcat(nombreArchivo, "-");
-        strcat(nombreArchivo, argv[index]);
+        if(validararchivo(argv[index])!=0){
+          strcat(nombreArchivo, "-");
+          strcat(nombreArchivo, argv[index]);
+        }else{
+          printf("El proceso %s no existe actualmente, por ende no se incluira en el archivo.\n", argv[index]);
+        }
       }
     strcat(nombreArchivo, ".info");
     //Se crea el archivo donde se escribirá la información de los procesos.
@@ -60,7 +67,7 @@ int main(int argc, char *argv[]){
        procesarProceso(argv[index], 1);
     }
     fclose(fpr);
-    printf("Archivo de salida generado: %s", nombreArchivo);
+    printf("Archivo de salida generado: %s\n", nombreArchivo);
   } else{ //Si no es ninguno de los casos anteriores, se dará por entendido que sólo ingresó el pid de un proceso por parámetro. En caso de haber ingresado más sólo tomará en cuenta el primero.
     procesarProceso(argv[1], 0);
   }
@@ -72,7 +79,7 @@ Además dependiendo de la bandera imprimirá o escribirá en un archivo dichas c
 0 para imprimir en pantalla, cualquier otro valor para escribir en un archivo.
 */
 void procesarProceso(char *id, int bandera){
-  FILE *fp;
+  
   Proceso proc = {
     "No disponible\n",
     "No disponible\n",
@@ -87,16 +94,9 @@ void procesarProceso(char *id, int bandera){
     "No disponible\n"
   };
   char linea[200];
-  char ruta[80];
-  //Se iniciliza la ruta del archivo que contiene la información del estado del proceso.
-  strcpy(ruta, "/proc/");
-  strcat(ruta, id );
-  strcat(ruta, "/status" );
-  //Se abre el archivo.
-  fp = fopen(ruta, "r");
-  if (fp == NULL){
-    printf("El proceso %s no existe actualmente.\n", ruta);
-    exit(1);
+  if(validararchivo(id)==0){
+   printf("El archivo %s no se encuentra actualmente, es probable que el proceso %s no exista.\n\n", ruta, id);
+   return;
   }
   //Se inicializa el PID del proceso.
   strcpy(proc.id, id);
@@ -104,7 +104,7 @@ void procesarProceso(char *id, int bandera){
   //Se recorre el archivo línea por línea para capturar la información necesaria.
   while(fgets(linea, 200, fp) != NULL){
     //La variable dato contedrá sólo la información del parámetro (se elimina el título: name, state, etc...).
-    char *dato, *h;
+    char *dato;
     dato = strchr(linea, 9); //9 es el codigo ASCII para el tab horizontal.
     dato++;
     if(strncmp(linea, "Name", 4) == 0){
@@ -146,4 +146,22 @@ void imprimirProceso(Proceso p){
 void guardarProceso(Proceso p){
   fprintf(fpr, "Pid: %sEl nombre del proceso es: %sEl estado del proceso es: %sEl tamano total de imagen en memoria es: %s Tamano de la memoria en la region TEXT es: %s Tamano de la memoria en la region DATA es: %s Tamano de la memoria en la region STACK es: %sNumero de cambios de contexto realizados:\n Voluntarios: %s No voluntarios: %s\n", 
           p.id, p.nombre, p.estado, p.tam.total, p.tam.text, p.tam.data, p.tam.stack, p.cambiosv, p.cambiosi);
+}
+
+//Este metodo validará si el archivo que se desea abrir existe, recibe como parametro el id ingresado por el usuario para cada proceso y retornará 1 en caso de que exista y 0 en caso contrario.
+
+int  validararchivo(char *id){
+   
+  //Se iniciliza la ruta del archivo que contiene la información del estado del proceso.
+  strcpy(ruta, "/proc/");
+  strcat(ruta, id );
+  strcat(ruta, "/status" );
+  //Se abre el archivo.
+  fp = fopen(ruta, "r");
+  if (fp == NULL){
+    
+    //exit(1);
+    return 0;
+  }
+  return 1;
 }
